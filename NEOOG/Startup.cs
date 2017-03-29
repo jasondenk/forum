3bba67718881292ce27221572c32ead0fd0af8cd
jasondenk <jdenk9@gmail.com>
2017-03-29 17:23:51 -0400

    initial commit

---
+++ NEOOG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NEOOG.Startup))]
+namespace NEOOG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
